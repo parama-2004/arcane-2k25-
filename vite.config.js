9f0f11334a571aced5316c31e3ceb0c4b3e83b3f
Paramaguru S <kkhailash5@gmail.com>
2025-09-08 01:16:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "f237ec5c108e.ngrok-free.app" // 👈 Add your ngrok domain here
-    ]
-  },
+ 
   plugins: [react(), tailwindcss()],
-  base: "/arcane-2k25",
+  base: "/arcane-2k25-/",
 })
